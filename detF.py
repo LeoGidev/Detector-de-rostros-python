74c566029c9d0aa90cea4b60cd7d32152dd44b82
LeoGidev <leogimenez.dev@gmail.com>
2024-05-08 16:09:13 -0300

    se trabaja en estructura

--- detF.py
+++ detF.py
@@ -3,3 +3,7 @@ import cv2
 # Carga la imagen
 image_path = 'imagen.jpg'
 image = cv2.imread(image_path)
+
+# Crea el clasificador de rostros
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
