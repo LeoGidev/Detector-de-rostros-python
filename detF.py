4aaeabaae20343bc64cf58bf8af05b3481aa5736
LeoGidev <leogimenez.dev@gmail.com>
2024-05-08 16:10:31 -0300

    se trabaja en estructura

--- detF.py
+++ detF.py
@@ -9,3 +9,6 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 
 # Convierte la imagen a escala de grises
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detecta rostros en la imagen
+faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
